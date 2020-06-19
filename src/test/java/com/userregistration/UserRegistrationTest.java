c80728c326d7a01b2445c50b95cad64383fcc866
“Shubham” <shubhampattar2@gmail.com>
2020-06-19 12:30:53 +0530

    ADDED Test Case to Validate Password by Rule2

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -55,13 +55,13 @@ public class UserRegistrationTest {
     @Test
     public void givenPassword_WhenProper_ShouldReturn_True() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validatePassword("12345678");
+        boolean result = userValidater.validatePassword("1234AA5678");
         Assert.assertEquals(true,result);
     }
     @Test
     public void givenPassword_WhenNotProper_ShouldReturn_False() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validatePassword("121");
+        boolean result = userValidater.validatePassword("123asdfgh");
         Assert.assertEquals(false,result);
     }
 }
