3a673f1a440a271d478dc6f3cb89d4ed64a68cf8
“Shubham” <shubhampattar2@gmail.com>
2020-06-19 11:32:22 +0530

    ADDED Test Case For Validating First Name

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -1,5 +1,19 @@
 package com.userregistration;
 
-public class UserRegistrationTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturn_True() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateFirstName("Shubham");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturn_False() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateFirstName("Sh");
+        Assert.assertEquals(false,result);
+    }
 }
