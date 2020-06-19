3fe9ecc5a2662ef045346f1646a84b0c16e4d3bc
“Shubham” <shubhampattar2@gmail.com>
2020-06-19 11:58:22 +0530

    ADDED Test Case to Validate Email

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -28,4 +28,16 @@ public class UserRegistrationTest {
         boolean result = userValidater.validateLastName("Pa");
         Assert.assertEquals(false,result);
     }
+    @Test
+    public void givenEmail_WhenProper_ShouldReturn_True() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateEmail("abc.xyz@bl.co.in");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void givenEmail_WhenNotProper_ShouldReturn_False() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateEmail("shubham@.com");
+        Assert.assertEquals(false,result);
+    }
 }
