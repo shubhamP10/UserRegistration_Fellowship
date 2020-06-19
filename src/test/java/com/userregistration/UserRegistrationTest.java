6c750798a7cdccd23c939b6cb003bf0c3adeac57
“Shubham” <shubhampattar2@gmail.com>
2020-06-19 13:04:05 +0530

    WIP for Password Rule 4

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -55,13 +55,13 @@ public class UserRegistrationTest {
     @Test
     public void givenPassword_WhenProper_ShouldReturn_True() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validatePassword("abcdfdAfg0");
+        boolean result = userValidater.validatePassword("abcdfdA@#fg0");
         Assert.assertEquals(true,result);
     }
     @Test
     public void givenPassword_WhenNotProper_ShouldReturn_False() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validatePassword("adasdasdaS");
+        boolean result = userValidater.validatePassword("adasdas0daS");
         Assert.assertEquals(false,result);
     }
 }
